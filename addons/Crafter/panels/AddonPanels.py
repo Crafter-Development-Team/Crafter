f153d896a66983ce79faa9ae49f25020f6b658f5
少年忠城 <121702811+snzhongcheng@users.noreply.github.com>
2024-12-26 02:03:38 +0800

    定义导入材质UI面板

--- addons/Crafter/panels/AddonPanels.py
+++ addons/Crafter/panels/AddonPanels.py
@@ -5,8 +5,8 @@ from ....common.i18n.i18n import i18n
 from ....common.types.framework import reg_order
 
 @reg_order(0)#==========导入世界面板==========
-class VIEW3D_PT_ImportWorld(bpy.types.Panel):
-    bl_idname = "VIEW3D_PT_ImportWorld"
+class VIEW3D_PT_CrafterImportWorld(bpy.types.Panel):
+    bl_idname = "VIEW3D_PT_CrafterImportWorld"
     bl_label = "Import World"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
@@ -19,14 +19,27 @@ class VIEW3D_PT_ImportWorld(bpy.types.Panel):
         layout.prop(addon_prefs, "World_Path")
         layout.prop(addon_prefs, "XYZ_1")
         layout.prop(addon_prefs, "XYZ_2")
-        layout.operator("scene.crafter_import_world")
+        layout.operator("crafter.import_world")
 
+@reg_order(1)#==========导入纹理面板==========
+class VIEW3D_PT_CrafterImportResource(bpy.types.Panel):
+    bl_idname = "VIEW3D_PT_CrafterImportResource"
+    bl_label = "Import Resource"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Crafter"
+    def draw(self, context: bpy.types.Context):
+        
+        layout = self.layout
+        addon_prefs = context.preferences.addons[__addon_name__].preferences
+
+        #layout.operator()
 
     @classmethod
     def poll(cls, context: bpy.types.Context):
-        return context.preferences.addons[__addon_name__].preferences.Import_World
+        return context.preferences.addons[__addon_name__].preferences.Import_Resource
 
-@reg_order(1)#==========加载材质面板==========
+@reg_order(2)#==========加载材质面板==========
 class VIEW3D_PT_Materials(bpy.types.Panel):
     bl_label = "Load Materials"
     bl_space_type = "VIEW_3D"
